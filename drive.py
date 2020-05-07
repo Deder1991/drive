0a3d1862a23f7cece50fb0c9d6054bb36ac9b985
Deder1991 <l22619025@gmail.com>
2020-05-07 15:26:06 +0800

    add other

--- drive.py
+++ drive.py
@@ -1,6 +1,6 @@
-country = input("input your country(台灣/美國):")
 age = input("inprt your age:")
 age = int(age)
+country = input("input your country(台灣/美國):")
 
 if country == "台灣":
 	if age >= 18:
@@ -12,4 +12,6 @@ elif country == "美國":
 		print("你可以考駕照")
 	else:
 		print("你還不可以考駕照")
-		
+else :
+	print("只能輸入台灣或美國")
+
